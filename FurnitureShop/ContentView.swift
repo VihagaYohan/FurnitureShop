0177c125bbb0511acb0dd4c1715f234763dba2ee
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-06-16 22:02:27 +0530

    Initial Commit

---
+++ FurnitureShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FurnitureShop
+//
+//  Created by Yohan on 2024-06-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FurnitureShop/FurnitureShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureShopApp.swift
+//  FurnitureShop
+//
+//  Created by Yohan on 2024-06-16.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
